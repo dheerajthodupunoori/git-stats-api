1363d211454d1a9cc67f1bdd74650fb5640ebdaf
Drj <dheeraj.thodupunoori01@gmail.com>
2021-08-08 22:33:30 +0530

    implemented sending email functionality

--- app/emailoperations/email_routes.py
+++ app/emailoperations/email_routes.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Body, status
 from .email_message_model import Email
-
 # from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
-# from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse
+import smtplib, ssl
 
 email_operations_router = APIRouter(
     tags=["Email Operations"],
@@ -10,16 +10,12 @@ email_operations_router = APIRouter(
 
 
 # conf = ConnectionConfig(
+#     MAIL_USERNAME="drj.poc@gmail.com",
 #     MAIL_PASSWORD="poc@0895",
-#     MAIL_FROM_NAME="Git stats",
-#     MAIL_FROM="drj.poc@gmail.com",
-#     MAIL_TLS=True,
-#     MAIL_SSL=False,
-#     USE_CREDENTIALS=True,
-#     MAIL_USERNAME="Dheeraj",
 #     MAIL_PORT=587,
 #     MAIL_SERVER="smtp.gmail.com",
-#     TEMPLATE_FOLDER=None
+#     MAIL_TLS=True,
+#     MAIL_SSL=False,
 # )
 
 
@@ -49,13 +45,21 @@ async def sendEmail(email: Email = Body(
             "email_body": "Body of email"
         }
     }
-)):
-    pass
-    # message = MessageSchema(
-    #     subject=email.email_subject,
-    #     recipients="dheeraj.thodupunoori01@gmail.com",
-    #     body=email.email_body + "from" + email.sender_email,
-    # )
-    # fm = FastMail(conf)
-    # await fm.send_message(message)
-    # return JSONResponse(status_code=200, content={"message": "email has been sent"})
+)) -> JSONResponse:
+    smtp_server = "smtp.gmail.com"
+    port = 587
+    sender_email = "drj.poc@gmail.com"
+    pwd = "poc@0895"
+    context = ssl.create_default_context()
+    try:
+        server = smtplib.SMTP(smtp_server, port)
+        server.starttls(context=context)
+        server.login(sender_email, pwd)
+        message = email.name + "\n" + email.sender_email + "\n" + \
+                  email.email_subject + "\n" + email.email_body
+        server.sendmail(sender_email, "dheeraj.thodupunoori01@gmail.com", message)
+        return JSONResponse(status_code=200, content={"message": "email has been sent"})
+    except Exception as error:
+        return JSONResponse(status_code=500, content={"message": error})
+    finally:
+        server.quit()
